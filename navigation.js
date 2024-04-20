a1238abbcd9c6c4bae5d0c23432481a9a7770394
yousef2468-1 <148258330+yousef2468-1@users.noreply.github.com>
2024-04-20 02:53:04 +0200

    Add files via upload

--- navigation.js
+++ navigation.js
@@ -1,19 +1,72 @@
-// Add this JavaScript code in a separate .js file, let's say navigation.js
 
-document.addEventListener('DOMContentLoaded', function() {
-  // Get all navigation links
-  const navLinks = document.querySelectorAll('nav a');
+// this functoin for search with book name or book author and display the data.
+function searchAndDisplayBookDetails(bookName) {
+  const query = bookName.toLowerCase(); 
+  const results = libraryBooks.filter(book => {
+      return book.title.toLowerCase().includes(query) || book.author.toLowerCase().includes(query);
+  }
+   );
 
-  // Add event listener to each navigation link
-  navLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
-      event.preventDefault(); // Prevent the default link behavior
+  if (results.length > 0) {
+      results.forEach(book => {
+    
+        console.log("Title:", book.title); 
 
-      // Get the target page URL from the href attribute of the clicked link
-      const targetUrl = link.getAttribute('href');
+          console.log("Author:", book.author);
 
-      // Navigate to the target page
-      window.location.href = targetUrl;
-    });
-  });
-});
+          console.log("Pages:", book.pages);
+
+          console.log("\n");
+    }
+      );
+        } 
+  
+  else {
+      console.log("Book not found.");
+  }
+    }
+
+
+// Function to check out a book if available.
+function checkOutBook(bookId) {
+
+  const book = libraryBooks.find(book => book.id === bookId);
+  if (book) {
+      if (book.available) {
+          book.available = false;
+          console.log("Book checked out successfully.");
+      } 
+      else {
+          console.log("Book is not available for checkout.");
+      }
+  } 
+  
+  else {
+      console.log("Book not found.");
+  }
+}
+
+
+//  and this Function to return a book 
+function returnBook(bookId) {
+
+  const book = libraryBooks.find(book => book.id === bookId);
+
+  if (book) {
+    if (!book.available) {
+         book.available = true;
+         console.log("Book returned successfully.");
+      } 
+    else {
+         console.log("Book is already available.");
+      }
+
+    }
+   else {
+      console.log("Book not found.");
+     }
+} 
+ 
+
+
+
